142f99536a445e34ec0b475ae36ce01a91b8eece
Mare Alexandra-Camelia <marekami234@gmail.com>
2024-11-18 08:41:08 +0200

    Add project files.

---
+++ MareAlexandra-CameliaLab7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MareAlexandra_CameliaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MareAlexandra-CameliaLab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MareAlexandra_CameliaLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
